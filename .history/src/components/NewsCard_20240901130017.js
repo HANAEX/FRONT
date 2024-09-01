5595f60193b18a08d91e34dc986e552d84bdf2f6
moho852 <moho191113@gmail.com>
2024-09-01 14:55:17 +0900

    dev:dbconnect

---
+++ .history/src/components/NewsCard_20240901130017.js
@@ -0,0 +1,50 @@
+// import React from 'react'
+// import { Text } from '@chakra-ui/react'
+// import { Link } from 'react-router-dom';
+
+// const NewsCard = ({ state, title, content, image_url, result }) => {
+//   return (
+//     <Link to={"/NewsPageDetail"} name="card-container" className='w-[300px] h-[290px] p-3 bg-slate-50 rounded-2xl'>
+//       <img src='/image/new_img.png' alt='News Image'  className='object-cover w-full h-[150px] rounded-xl'/>
+//       <div name="text-box" className='flex flex-col items-center'>
+//         <Text fontSize='lg' className='grow py-1' as={'b'}>
+//           { title }
+//         </Text>
+//         <Text fontSize={'sm'} lineHeight={1} className='h-[42px] bg-slate-400 grow text-center text-slate-600'>
+//           { content }
+//         </Text>
+//         <Text fontSize={'xl'} as={'b'} className='grow'>
+//           { result }
+//         </Text>
+//       </div>
+//     </Link>
+//   )
+// }
+
+// export default NewsCard
+
+import React from 'react'
+import { Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
+
+const NewsCard = ({ state, title, content, image_url, result }) => {
+  return (
+    <Link to={"/NewsPageDetail"} name="card-container" className='w-[300px] h-[290px] p-3 bg-slate-50 rounded-2xl'>
+      {console.console.log({ image_url })}
+      <img src= { image_url } alt='News Image'  className='object-cover w-full h-[150px] rounded-xl'/>
+      <div name="text-box" className='flex flex-col items-center'>
+        <Text fontSize='lg' className='grow py-1' as={'b'}>
+          { title }
+        </Text>
+        <Text fontSize={'sm'} lineHeight={1} className='h-[42px] bg-slate-400 grow text-center text-slate-600'>
+          { content }
+        </Text>
+        <Text fontSize={'xl'} as={'b'} className='grow'>
+          { result }
+        </Text>
+      </div>
+    </Link>
+  )
+}
+
+export default NewsCard
